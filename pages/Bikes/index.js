e0f711dcb837b5e3b02b87c999bdeeb36edc0163
Yuri Padilla <yuripadilla017@gmail.com>
2023-01-25 15:51:01 +0100

    add BikesPreview to page Home

---
+++ pages/Bikes/index.js
@@ -0,0 +1,7 @@
+export default function Bikes() {
+  return (
+    <>
+      <p>hallo</p>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import PriceInfo from "../components/PriceInfo";
+import BikesPreview from "../components/BikesPreview";
+import { bikes } from "../lib/bikes.js";
 
 export default function HomePage() {
   return (
     <>
       <PriceInfo />
+      <BikesPreview bikes={bikes} />
     </>
   );
 }
