e35c4e80b620f266d728b16250343f01cd53ddc7
Yuri Padilla <yuripadilla017@gmail.com>
2023-01-25 15:29:25 +0100

    create PriceInfo and array of bikes

---
+++ components/PriceInfo/index.js
@@ -0,0 +1,22 @@
+export default function PriceInfo() {
+  return (
+    <>
+      <h2>Lease Time & Price</h2>
+      <section>
+        <div>
+          <h3>Prices</h3>
+          <article>$4/hour</article>
+          <article>$15/day the first day (until 8:00 PM)</article>
+          <article>$10/day after the second day on</article>
+          <p>Bikes must be returned before 8:00 PM</p>
+        </div>
+        <div>
+          <h3>Hours</h3>
+          <article>Monday through Sunday</article>
+          <article>Monday through Sunday</article>
+          <article>8:00 AM to 8:00 PM</article>
+        </div>
+      </section>
+    </>
+  );
+}
