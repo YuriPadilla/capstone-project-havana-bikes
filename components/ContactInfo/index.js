bb93ace92635c30eb2b940a3b6bed936d3209fe7
Yuri Padilla <yuripadilla017@gmail.com>
2023-01-27 15:51:33 +0100

    small update

--- components/ContactInfo/index.js
+++ components/ContactInfo/index.js
@@ -12,7 +12,7 @@ export default function ContactInfo() {
           <article>
             61 Consulado St. between Genios and Refugio St. Centro Habana, Cuba.
           </article>
-          <article>Mobile: (+53) 5 306 2175</article>
+          <article>Mobile: (+53) 5 391 2608</article>
           <article>Land line: (+53) 7 867 6782</article>
           <p>email: yuripadilla017@gmail.com</p>
         </div>
