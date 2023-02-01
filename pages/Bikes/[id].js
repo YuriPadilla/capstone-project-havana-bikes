6b9b738e5a239ce49dda424e86a64a14c691c88b
Yuri Padilla <yuripadilla017@gmail.com>
2023-02-01 10:54:20 +0100

    add dynamic page [id].js

---
+++ pages/Bikes/[id].js
@@ -0,0 +1,26 @@
+import Header from "../../components/Header";
+import { bikes } from "../../lib/bikes.js";
+import { useRouter } from "next/router";
+import ProductDetails from "../../components/ProductDetails";
+import Link from "next/link";
+
+export default function Bike() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const currentBike = bikes.find((bike) => bike.id === id);
+
+  if (!currentBike) {
+    return null;
+  }
+
+  return (
+    <>
+      <Header />
+      <p>
+        <Link href="/">Home</Link>→<Link href="/Bikes">Bikes</Link>→Details
+      </p>
+      <ProductDetails product={currentBike} />
+    </>
+  );
+}
