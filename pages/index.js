8ded77bba23f326549c60d6eb4f05fc868d36c1e
Yuri Padilla <yuripadilla017@gmail.com>
2023-02-09 14:48:27 +0100

    remove component BikeParts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import PriceInfo from "../components/PriceInfo";
 import BikesPreview from "../components/BikesPreview";
 import { bikes } from "../lib/bikes.js";
-import BikePartsPreview from "../components/BikePartsPreview";
-import { bikeParts } from "../lib/bikeParts";
 import ContactInfo from "../components/ContactInfo";
 
 export default function HomePage() {
@@ -11,7 +9,6 @@ export default function HomePage() {
       <p>Home</p>
       <PriceInfo />
       <BikesPreview bikes={bikes} />
-      <BikePartsPreview bikeParts={bikeParts} />
       <ContactInfo />
     </>
   );
