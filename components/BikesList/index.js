ac5d607d0cae5088defaa12964d57d24bd1125a8
Yuri Padilla <yuripadilla017@gmail.com>
2023-02-15 15:12:02 +0100

    style page Bikes

--- components/BikesList/index.js
+++ components/BikesList/index.js
@@ -3,6 +3,24 @@ import Link from "next/link.js";
 import { StyledBikesUl, StyledBikeLi } from "./BikesList.styled";
 import useLocalStorageState from "use-local-storage-state";
 import useSWR from "swr";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  background-color: rgb(254, 254, 254);
+  padding: 20px;
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const StyledWrapperHeading = styled.div`
+  width: 100%;
+`;
+
+const StyledHeadingH3 = styled.h3`
+  margin: 0;
+  padding: 0;
+`;
 
 export default function BikesList() {
   const [selectedProducts] = useLocalStorageState("selectedProducts", {
@@ -26,28 +44,33 @@ export default function BikesList() {
 
   return (
     <>
-      <h2>Bikes</h2>
-      <StyledBikesUl>
-        {bikesToShow.length > 0 ? (
-          bikesToShow.map((bike) => {
-            return (
-              <StyledBikeLi key={bike._id}>
-                <Link href={`/Bikes/${bike._id}`}>
-                  <Image
-                    src={bike.imageSource}
-                    height={110}
-                    width={180}
-                    alt={bike.brand}
-                    priority
-                  />
-                </Link>
-              </StyledBikeLi>
-            );
-          })
-        ) : (
-          <p>No bikes to show</p>
-        )}
-      </StyledBikesUl>
+      <StyledSection>
+        <StyledWrapperHeading>
+          <StyledHeadingH3>Bikes</StyledHeadingH3>
+          <hr />
+        </StyledWrapperHeading>
+        <StyledBikesUl>
+          {bikesToShow.length > 0 ? (
+            bikesToShow.map((bike) => {
+              return (
+                <StyledBikeLi key={bike._id}>
+                  <Link href={`/Bikes/${bike._id}`}>
+                    <Image
+                      src={bike.imageSource}
+                      height={85}
+                      width={146}
+                      alt={bike.brand}
+                      priority
+                    />
+                  </Link>
+                </StyledBikeLi>
+              );
+            })
+          ) : (
+            <p>No bikes to show</p>
+          )}
+        </StyledBikesUl>
+      </StyledSection>
     </>
   );
 }
